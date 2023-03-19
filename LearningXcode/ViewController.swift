0e88ad0fa3426a2fe2d5f6a566e9344007e4185f
brozozowska <s_rozov@me.com>
2023-03-19 17:03:34 +0300

    Initial Commit

---
+++ LearningXcode/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  LearningXcode
+//
+//  Created by Сергей Розов on 21.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
